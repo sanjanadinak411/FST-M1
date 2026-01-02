5eb1a55a0292684de82a403d9bc284f779a64001
sanjanadinak411 <119032523+sanjanadinak411@users.noreply.github.com>
2026-01-02 16:38:05 +0530

    Add files via upload

--- Appium/Project/activity1.java
+++ Appium/Project/activity1.java
@@ -1,63 +1,41 @@
-package project1;
+package project2;
 
-import static org.testng.Assert.assertEquals;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class activity1 extends Base{
 	
 	@Test(priority=0)
 	public void method1() {
 		
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.app.todolist:id/fab_new_task"))).click();
-		driver.findElement(AppiumBy.id("com.app.todolist:id/et_new_task_name")).click();
-		driver.findElement(AppiumBy.id("com.app.todolist:id/et_new_task_name")).sendKeys("Activity 1");
-		driver.findElement(AppiumBy.id("com.app.todolist:id/et_new_task_description")).sendKeys("Complete Activity 1 with priority high");
-		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_new_task_ok")).click();
-		result();
-	}
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true))"+".scrollIntoView(new UiSelector().textContains(\"To-Do List\"))"+".scrollToEnd(100,3)")).click();
+		
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='todo-input']")).click();
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='todo-input']")).sendKeys("Add tasks to list");
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@resource-id='todo-add']"))).click();
+		
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='todo-input']")).sendKeys("Get number of tasks");
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@resource-id='todo-add']"))).click();
 	
-	@Test(priority=1)
-	public void method2() {
-		
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.app.todolist:id/fab_new_task"))).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='com.app.todolist:id/et_new_task_name']")).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='com.app.todolist:id/et_new_task_name']")).sendKeys("Activity 2");
-		driver.findElement(AppiumBy.id("com.app.todolist:id/et_new_task_description")).sendKeys("Complete Activity 2 with priority medium");
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id='com.app.todolist:id/tv_new_task_priority']")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@resource-id='android:id/title' and @text='Medium']"))).click();
-		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_new_task_ok")).click();	
-		result();
-		
-	}
-	@Test(priority=2)
-	public void method3() {
-		
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.app.todolist:id/fab_new_task"))).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='com.app.todolist:id/et_new_task_name']")).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='com.app.todolist:id/et_new_task_name']")).sendKeys("Activity 3");
-		driver.findElement(AppiumBy.id("com.app.todolist:id/et_new_task_description")).sendKeys("Complete Activity 3 with priority low");
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id='com.app.todolist:id/tv_new_task_priority']")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@resource-id='android:id/title' and @text='Low']"))).click();
-		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_new_task_ok")).click();	
-		result();
-	}
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='todo-input']")).sendKeys("Clear the list");
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@resource-id='todo-add']"))).click();
 	
+		List<WebElement> count=driver.findElements(AppiumBy.className("android.widget.CheckBox"));
+		for(WebElement item:count) {
+			item.click();
+		}
+		
+		System.out.println(count.size());
+		Assert.assertEquals(count.size(),5);
+	}
+
 }
--- Appium/Project/activity2.java
+++ Appium/Project/activity2.java
@@ -1,48 +1,39 @@
-package project1;
+package project2;
 
-import static org.testng.Assert.assertTrue;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.Set;
 
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.functions.ExpectedCondition;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.SupportsContextSwitching;
 
 public class activity2 extends Base{
-
 	
-	@Test(priority=3)
-	public void method4() {
+	
+	@Test(priority=1)
+	public void method2() {
+	
+		//WebElement card1=wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Please sign in!")));+".scrollToEnd(15)"
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true))"+".scrollIntoView(new UiSelector().textContains(\"Login Form\"))")).click();
 		
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@resource-id='com.app.todolist:id/tv_exlv_task_name' and @text='Activity 2']")));
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='username']")).sendKeys("admin");
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@resource-id='password']")).sendKeys("password");
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.Button[@text='Submit']"))).click();
 		
-		Dimension dim=driver.manage().window().getSize();
-		
-		int StartX=(int) (0.26f* dim.getWidth());
-		int StartY=(int) (0.17f* dim.getHeight());
-		
-		Point start=new Point(StartX,StartY);
-		
-		press(driver,start,4000);
-		
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id='android:id/title' and @text='Edit To-Do Task']")).click();
-		driver.findElement(AppiumBy.id("com.app.todolist:id/tv_todo_list_deadline")).click();
-		
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("android:id/next"))).click();
-		
-		driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc='03 January 2026']")).click();	
-		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_deadline_ok")).click();
-		
-		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_new_task_ok")).click();	
-		String date=wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.TextView[@resource-id='com.app.todolist:id/tv_exlv_task_deadline' and @text='Deadline: 03.01.2026']"))).getText();
-		
-		System.out.println(date);
-		Assert.assertEquals(date,"Deadline: 03.01.2026"); 
+		String result1=wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='Login Success!']"))).getText();
+		Assert.assertEquals(result1,"Login Success!");
+	
 	}
-	
 	
 }
